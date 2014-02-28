0e9b081233ac1b22a7e15b0975ee82bd745e5eb9
David H <djh@infowest.com>
2014-02-27 17:16:28 -0800

    Seed Complete

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,14 @@
+require "digest/md5"
+
+module ApplicationHelper
+
+  def gravatar_url(email, options={})
+    digest = Digest::MD5.hexdigest(email)
+    url_options = {host: "www.gravatar.com/avatar/#{digest}", d: "retro"}.merge(options)
+    url = url_for(url_options)
+    url
+
+  end
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Facespace::Application.config.secret_key_base = 'c581078a4700f4471c7d89aedd083497664b783fb4a3c81a6955422f4d6a3326396fa6675a7356599386cbf74964c6bfa34ab946984dd8424c3f0d2062501ab7'
